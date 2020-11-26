65b7a38c0f8d69fce7360227b4aa45b69cdf0ab5
rootz491 <experimentyt1@gmail.com>
2020-11-26 22:55:51 +0530

    changed something

--- website/settings.py
+++ website/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
