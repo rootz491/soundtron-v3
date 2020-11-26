06d8d6e68902412f378ca097d6a22bad0433a1d3
rootz491 <experimentyt1@gmail.com>
2020-11-26 23:04:36 +0530

    removed DEBUG restrictions

--- website/urls.py
+++ website/urls.py
@@ -27,7 +27,6 @@ urlpatterns = [
     #path(r'', views.index, name='index'),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
