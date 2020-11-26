2f30d2f2d0b2b0935cc715d165322616409b9a46
rootz491 <experimentyt1@gmail.com>
2020-11-26 22:38:16 +0530

    changed static file link

--- website/settings.py
+++ website/settings.py
@@ -120,10 +120,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 # Activate Django-Heroku.
